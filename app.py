607a9de010f362f7a7bafd4c0773f10afaeb77d4
sradms0 <sradms0@openmailbox.org>
2019-07-22 16:53:52 -0400

    add clean_data

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python3
 
 import constants
+from math import ceil
+
+def clean_data():
+    # calculate players per team
+    per_team = ceil(len(constants.PLAYERS) / len(constants.TEAMS))
+    teams = []
+
+    # init ranges for equally splitting players to teams
+    start = 0
+    stop = per_team
+    for t_idx, team in enumerate(constants.TEAMS):
+        players = constants.PLAYERS[start:stop]
+
+        for player in players:
+            # clean up height and experience data
+            name, guardians, experience, height = player.values()
+            player['height'] = int(height[:2])
+            player['experience'] = True if experience == 'YES' else False
+
+        # add new team with players
+        teams.append({'team': team, 'players': players})
+
+        # update ranges
+        start = start+per_team
+        stop = start+per_team
+
+    return teams
+
 
 if __name__ == '__main__':
-    print(f'testing: {__name__}')
+    for dat in clean_data():
+        team = dat['team']
+        players = dat['players']
+
+        print(f'Team: {team}')
+        print('Players:')
+        for player in players: print(player)
+        print()
+
+
