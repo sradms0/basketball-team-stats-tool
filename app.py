8059e482611d9ca46a83b30af3ce84d238a32c1d
sradms0 <sradms0@openmailbox.org>
2019-07-23 13:18:30 -0400

    move in/experienced count to clean_data

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def clear_screen():
 def clean_data():
     # calculate players per team
     per_team = ceil(len(constants.PLAYERS) / len(constants.TEAMS))
+    experienced = 0
+    inexperienced = 0
     teams = []
 
     # init ranges for equally splitting players to teams
@@ -18,21 +20,31 @@ def clean_data():
     for t_idx, team in enumerate(constants.TEAMS):
         players = constants.PLAYERS[start:stop]
 
+        data = {'team': team, 'players': None, 'experienced': 0, 'inexperienced': 0}
         for player in players:
             # clean up experience, guardians, and height data
             name, guardians, experience, height = player.values()
-            player['experience'] = True if experience == 'YES' else False
+            experience = True if experience == 'YES' else False
+
+            player['experience'] = experience
             player['guardians'] = guardians.split(' and ')
             player['height'] = int(height[:2])
 
+            if experience: data['experienced'] += 1
+            else: data['inexperienced'] += 1
+
         # add new team with players
-        teams.append({'team': team, 'players': players})
+        data['players'] = players
+        teams.append(data)
 
         # update ranges
         start += per_team
         stop = start+per_team
 
     return teams
+
+def show_all_teams(teams):
+    for i, t in enumerate(teams): show_stats(teams, i)
 
 def main_menu():
     choices = ['display team stats', 'quit']
@@ -46,11 +58,9 @@ def stats_menu(teams):
     for i, t in enumerate(team_names, 1): print(f'{i}) {t}')
 
 def show_stats(teams, idx):
-    team, players = teams[idx].values()
+    team, players, experienced, inexperienced = teams[idx].values()
     player_names = []
     guardian_names = []
-    experienced = 0
-    inexperienced = 0
     total_height = 0
 
     # collect/calculate data to display
@@ -58,9 +68,6 @@ def show_stats(teams, idx):
         player_names.append(p['name'])
         guardian_names += p['guardians']
         total_height += p['height']
-
-        if p['experience']: experienced += 1
-        else: inexperienced += 1
 
     # format display strings
     header = f'Team: {team} Stats'
@@ -95,6 +102,9 @@ if __name__ == '__main__':
             stats_menu(teams)
             team_choice = prompt()
             show_stats(teams, team_choice-1)
+            cont() 
+        if main_choice == 3: 
+            show_all_teams(teams)
             cont() 
 
         clear_screen()
